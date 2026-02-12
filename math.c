4af9451c3d7500693687c7154edbc661fc68c6b0
Rouven Blättler <rouvenblattler@gmail.com>
2026-02-12 14:13:38 +0100

    test math functions in c

--- math.c
+++ math.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main() {
 
-    int x = 9;
+    float x = 45;
 
-    x = sqrt(x);
-    //x = pow(x, 3);
+    //x = sqrt(x);
+    // x = pow(x, 3);
+    //x = ceil(x);
+    // x = round(x);
+    //x = floor(x);
+    //x = abs(x);
+    //x = log(x);
+    //x = sin(x);
+    //x = cos(x);
 
-
-
-    printf("%d\n", x);
+    printf("%f\n", x);
 
     return 0;
 }
