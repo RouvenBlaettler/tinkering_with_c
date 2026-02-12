0d92089637879b09a222613f3375dbc2152ef998
Rouven Blättler <rouvenblattler@gmail.com>
2026-02-12 16:10:17 +0100

    experiment with if

---
+++ ifStatement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool student = true;
+
+    if(student){
+        printf("You are a student");
+    }
+    else{
+        printf("You aren't a student");
+    }
+    printf("\n");
+
+    return 0;
+}
