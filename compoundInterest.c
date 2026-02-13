4d33bc4889a9645d78b752b965e9c8a187a1197d
Rouven Blättler <rouvenblattler@gmail.com>
2026-02-13 17:27:55 +0100

    play with functions and arguments

--- compoundInterest.c
+++ compoundInterest.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+
+void hbd(char name[], int age){
+    printf("Happy Birthday %s %d\n", name, age);
+}
 
 int main() { // && || !
-    
-    int isSunny = true;
 
-    if(!isSunny){
-        printf("It is cloudy");
-    }
-    else{
-        printf("It's sunny");
-    }
+    char name[50] = "";
+    int age = 0;
+
+    printf("type your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name)-1] = '\0';
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    
+    hbd(name, age);
+    hbd(name, age);
+    hbd(name, age);
+    hbd(name, age);
 
     return 0;
 
