6b4f98d84762dc54a6d81bd6f20d3402ef3674c8
iFarmApples <rouvenblattler@gmail.com>
2026-02-19 15:04:36 +0100

    work with structs and enums

--- claculate.c
+++ claculate.c
@@ -1,19 +1,35 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdbool.h>
 
 typedef int Number;
 typedef char String[50];
-typedef char Initials[3];
+typedef enum{
+    MONDAY, TUESDAY, WENDSDAY, THURSDAY, FRIDAY, SATURDAY, SUNNDAY
+}days;
+typedef struct{
+    char name[50];
+    int age;
+    float gpa;
+    bool fullTime;
+}Student;
 
 int main() {
     
     Number x = 7;
 
-    Initials user = "RB";
-
     String name = "Bro Code";
 
+    days today = WENDSDAY;
 
+    Student peter = {"Peter", 12, 3.3, true};
+    Student tim = {0};
+
+    printf("%d\n", today);
+    printf("%s\n", peter.name);
+    printf("%d\n", peter.age);
+    printf("%f\n", peter.gpa);
+    printf("%s\n", (peter.fullTime) ? "yes" : "no");
     return 0;
 
     
