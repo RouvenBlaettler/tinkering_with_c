e2b5e661889336df69e2da9b180fce8bd87a9a68
iFarmApples <rouvenblattler@gmail.com>
2026-02-22 14:00:51 +0100

    wokr with pointers

--- claculate.c
+++ claculate.c
@@ -2,37 +2,24 @@
 #include<math.h>
 #include<stdbool.h>
 
-typedef int Number;
-typedef char String[50];
-typedef enum{
-    MONDAY, TUESDAY, WENDSDAY, THURSDAY, FRIDAY, SATURDAY, SUNNDAY
-}days;
-typedef struct{
-    char name[50];
-    int age;
-    float price;
-}Car;
+void birthday(int* age);
 
 int main() {
-    
-    Number x = 7;
+   int age = 25;
+   int *pAge = &age;
 
-    String name = "Bro Code";
+   birthday(pAge);
 
-    days today = WENDSDAY;
+   printf("You are %d years old\n", age);
 
-    Car cars[] = {{"turbo", 2025, 1000.00}, {"cleano", 2020, 10500.00}};
-
-    int number = sizeof(cars) / sizeof(cars[0]);
-
-    for(int i = 0; i < number; i++){
-        printf("%s, %d, %f\n", cars[i].name, cars[i].age, cars[i].price);
-    }
-
-
-    
 
     return 0;
 
     
+}
+
+void birthday(int* age){
+    
+    (*age)++;
+
 }
