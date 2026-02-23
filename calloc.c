22ca087134609823a2faf6255cf116934260b7c2
iFarmApples <rouvenblattler@gmail.com>
2026-02-23 21:16:46 +0100

    play with calloc

---
+++ calloc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int number = 0;
+    printf("Enter the number of players: ");
+    scanf("%d", &number);
+
+    int *scores = calloc(number, sizeof(int));
+
+    if(scores == NULL){
+        printf("Memory allocation failed");
+        return 1;
+    }
+
+    for(int i = 0; i < number; i++){
+        printf("Enter score number #%d: ", i + 1);
+        scanf("%d", &scores[i]);
+    }
+
+    for(int i = 0; i < number; i++){
+        printf("%d ", scores[i]);
+    }
+    printf("\n");
+
+    free(scores);
+    scores = NULL;
+
+
+
+    return 0;
+}
